ceee029bcc777d86cbbc7d45aa04cad29710a08f
joliencremers <joliencremers@gmail.com>
2017-10-24 11:03:21 +0200

    data update

--- data_raw/Motor Cognition/CreateRdata.R
+++ data_raw/Motor Cognition/CreateRdata.R
@@ -1,10 +1,10 @@
-Motor <- haven::read_spss(file = "data-raw/Motor Cognition/PhaseDifference.sav")
+Motor <- haven::read_spss(file = "data_raw/Motor Cognition/PhaseDifference.sav")
 Motor <- Motor[,1:3]
 
 Motor <- cbind(Motor, Motor$PhaseDiff*(pi/180))
 colnames(Motor) <- c("Cond", colnames(Motor[,-c(1,4)]), "Phaserad")
 
-Motor$Cond <- factor(Motor$Cond, labels = c("imp", "semi.imp", "exp"))
+Motor$Cond <- factor(Motor$Cond, labels = c("exp", "semi.imp", "imp"))
 Motor$AvAmp <- Motor$AvAmp - mean(Motor$AvAmp)
 
 devtools::use_data(Motor)
