463ed0354593ab95e56600880b02583fa7a93ec8
joliencremers <joliencremers@gmail.com>
2017-10-27 10:03:29 +0200

    data update

--- data_raw/Cognitive Maps/CreateRdata.R
+++ data_raw/Cognitive Maps/CreateRdata.R
@@ -1,7 +1,7 @@
 Maps <- haven::read_spss(file = "data-raw/Cognitive Maps/WarrenExp1.sav")
 
 Maps$Error.rad <- (Maps$Error/180)*pi
-Maps$Learn.c <- Maps$Learn-mean(Maps$Learn)
+Maps$L.c <- Maps$Learn-mean(Maps$Learn)
 Maps$Maze <- as.factor(Maps$Maze)
 Maps$Trial.type <- as.factor(Maps$Trial.type)
 
