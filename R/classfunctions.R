a55e5d0e52c9f4de2ae78416e0e4e8c4641c14aa
joliencremers <joliencremers@gmail.com>
2020-12-15 11:25:11 +0100

    documentation update

--- R/classfunctions.R
+++ R/classfunctions.R
@@ -972,7 +972,7 @@ print.bpnme <- function(x, ...){
 #' library(bpnreg)
 #' fit.Motor <- bpnr(pred.I = Phaserad ~ 1 + Cond, data = Motor,
 #' its = 100, burn = 10, n.lag = 3)
-#' traceplot(fit.Motor, parameter = "B1")
+#' traceplot(fit.Motor, parameter = "beta1")
 #'
 #' @export
 #'
